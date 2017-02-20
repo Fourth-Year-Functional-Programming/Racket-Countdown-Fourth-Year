37154d173cad8d4152735747fa578415e220adb3
Christy <christymadden80@gmail.com>
2017-02-20 19:09:54 +0000

    File created for the two number version of the game and comments explaining the purpose of the file

---
+++ two_number_version.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+#|
+    This file is used to have a 2 number version of the countdown game working.
+    The arithmetic operators will be used to get two numbers equal a target number.
+    The operations that can be used on the two numbers are +, -, / and *.
+|#
+
+#|  Example of what the program will do
+    1. Have a target number E.G 100
+    2. Have a list of two numbers which will be used to create the target number E.G 2, 50
+    3. Have the program work out if the two numbers can equal the target number when using them with the operators allowed
+|#
