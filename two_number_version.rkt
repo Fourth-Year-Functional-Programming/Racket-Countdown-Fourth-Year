296cc7d15c155a0a89d2bcc0f60e97f124ef256c
Christy <christymadden80@gmail.com>
2017-02-24 16:59:10 +0000

    Variables defined and all possible calculations for 2 numbers added to file

--- two_number_version.rkt
+++ two_number_version.rkt
@@ -11,3 +11,14 @@
     2. Have a list of two numbers which will be used to create the target number E.G 2, 50
     3. Have the program work out if the two numbers can equal the target number when using them with the operators allowed
 |#
+
+#|
+    ==========  VARIABLES  ==========
+|#
+
+;   Simple hard coded target number for start of development
+(define target 100)
+
+;   The two numbers which have to be calculated to make the target number
+(define a 50)
+(define b 2)
