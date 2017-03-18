8acb76009d1a873ead3cf8aa3e6d4c8a79d69747
Christy <christymadden80@gmail.com>
2017-03-18 14:13:18 +0000

    Testing functions which could be of use for the final working program

--- two_number_version.rkt
+++ two_number_version.rkt
@@ -22,3 +22,49 @@
 ;   The two numbers which have to be calculated to make the target number
 (define a 50)
 (define b 2)
+
+;   Simple function to add two numbers together
+(define (sum x y) (+ x y))
+
+#|
+    List of all the calculations that can be done with the 2 number version of the game 
+
+    (a + b)
+    (b + a)
+    (a - b)
+    (b - a)
+    (a * b)
+    (b * a)
+    (a / b)
+    (b / a)
+|#
+
+#|
+    Pick random 6 numbers to get permutations
+|#
+
+;(remove-duplicates (permutations (list 10 50)))
+
+; This gives all the permutations of the list
+;(permutations (list 10 50))
+
+; This was used to see if a list of operators could be output the same way as a list of numbers
+;(permutations (list '+ '- '/ '*))
+
+; This code was used to see and test the workings of the cartesian-product function
+;(cartesian-product (list 10 50) (list '+ '- '/ '*))
+
+; This was an alternative piece of code compared to the above code 
+(cartesian-product(permutations (list 10 50 '+ '- '/ '*)))
+
+
+
+
+
+
+
+
+
+
+
+
