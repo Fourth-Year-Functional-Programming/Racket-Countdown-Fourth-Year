a61f3aedbb3a5670abcc70a2ac06cfbc420e5b2f
Christy <christymadden80@gmail.com>
2017-04-17 20:19:46 +0100

    Calcuate combinations for 2 and 3 numbers

--- combinations.rkt
+++ combinations.rkt
@@ -3,6 +3,9 @@
 #|
     This file will show use of the combination function and how it will work
     on a 6 number list.
+
+    Calculations are referenced from
+    http://www.statisticshowto.com/calculators/permutation-calculator-and-combination-calculator/
 |#
 
 ;  A 6 number list to represent the numbers you could find on countdown
@@ -10,4 +13,14 @@
 
 ; Find all the combinations with 2 numbers
 ; There will be 15 combinations
-(combinations c-list 2)
+; This is calculted as follows
+; 6! / 2!(6 – 2)!
+; 720 / 48 = 15
+;(combinations c-list 2)
+
+; Find all the combinations with 2 numbers
+; There will be 20 combinations
+; This is calculted as follows
+; 6! / 3!(6 – 3)!
+; 720 / 36 = 20
+(combinations c-list 3)
