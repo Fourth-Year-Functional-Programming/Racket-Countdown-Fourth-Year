6364a08a0dba76808ac1cb7f7a4b06fd5cc67479
Christy <christymadden80@gmail.com>
2017-04-17 20:24:42 +0100

    Calcuate combinations for 4 numbers

--- combinations.rkt
+++ combinations.rkt
@@ -18,9 +18,16 @@
 ; 720 / 48 = 15
 ;(combinations c-list 2)
 
-; Find all the combinations with 2 numbers
+; Find all the combinations with 3 numbers
 ; There will be 20 combinations
 ; This is calculted as follows
 ; 6! / 3!(6 – 3)!
 ; 720 / 36 = 20
-(combinations c-list 3)
+;(combinations c-list 3)
+
+; Find all the combinations with 4 numbers
+; There will be 20 combinations
+; This is calculted as follows
+; 6! / 4!(6 – 4)!
+; 720 / 48 = 15
+(combinations c-list 4)
