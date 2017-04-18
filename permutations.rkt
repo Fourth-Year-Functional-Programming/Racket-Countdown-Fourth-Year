527f857b7054aeb63ca5a0b616866036a5c5f1c8
Christy <christymadden80@gmail.com>
2017-04-18 20:55:16 +0100

    Add permutations for 10 element list calculated

--- permutations.rkt
+++ permutations.rkt
@@ -11,10 +11,27 @@
 ;  A 6 number list to represent the numbers you could find on countdown
 (define p-list (list 1 2 3 4 25 50))
 
+; A list which contains the 6 numbers and the operators which can be used
+(define op-list (list 1 2 3 4 25 50 + - / *))
+
 
 ; Find all the permutations of a list with 6 numbers
+; There are no duplicates
 ; There will be 720 permutations
 ; This is calculted as follows
 ; 6! / (6 – 6)!
 ; 720 / 1 = 720
-(permutations p-list)
+;(permutations p-list)
+
+; Will print out the number of permutations that are found in a 10 element list
+;(length (permutations op-list))
+
+; Find all the permutations of a list with 10 elements
+; There are no duplicates
+; There will be 3628800 permutations
+; This is calculted as follows
+; 10! / (10 – 10)!
+; 3628800 / 1 = 3628800
+(length(permutations op-list))
+
+
