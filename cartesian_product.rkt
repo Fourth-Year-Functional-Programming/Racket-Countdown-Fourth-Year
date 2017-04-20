502cc4158464220463602df5e61dafe3a950d8c1
Christy <christymadden80@gmail.com>
2017-04-20 20:13:59 +0100

    Add cartesian-product function demonstration

--- cartesian_product.rkt
+++ cartesian_product.rkt
@@ -1,1 +1,18 @@
 #lang racket
+
+#|
+    This file is to demonstrate the use of the funtion cartesian-product.
+    It's a function which combines the elements of two lists
+    E.g Take two seperate lists (a,b) and (1,2)
+    Expected Result: (a,1)(a,2)(b,1)(b,2)
+|#
+
+; A list which could be found in the countdown game
+(define n-list (list 1 2 3 4 25 50))
+
+; A list of operators allowed in countdown game
+(define op-list (list '+ '- '/ '*))
+
+; The combination of elements from each list
+(cartesian-product n-list op-list)
+
