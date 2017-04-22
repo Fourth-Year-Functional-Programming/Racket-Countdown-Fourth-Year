970387751c42838eb6c88df9a3ee18e0e1e173eb
Christy <christymadden80@gmail.com>
2017-04-22 12:58:31 +0100

    Add fuction to test a negative number and return true or false

---
+++ if_statements.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+#|
+    This file is used to have working versions of if statements
+|#
+
+; define a negative value
+(define neg-num -10)
+
+; Function to return true or false if number is negative
+; Can be found at https://docs.racket-lang.org/reference/if.html
+; Function should retrun false 
+(positive? neg-num)
