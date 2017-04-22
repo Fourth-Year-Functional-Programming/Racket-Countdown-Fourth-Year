2470cef119af16ce63abb29d4af3cd492876d1d8
Christy <christymadden80@gmail.com>
2017-04-22 13:13:06 +0100

    Add simple working program with if statement for demonstration

--- if_statements.rkt
+++ if_statements.rkt
@@ -1,13 +1,24 @@
 #lang racket
 
 #|
-    This file is used to have working versions of if statements
+    This file is used to have working versions of if statements.
+    Functions and information can be found at the link below
+    https://docs.racket-lang.org/reference/if.html
 |#
 
 ; define a negative value
 (define neg-num -10)
 
+; A string to give a positive result
+(define success "Number is positive")
+
 ; Function to return true or false if number is negative
-; Can be found at https://docs.racket-lang.org/reference/if.html
 ; Function should retrun false 
 (positive? neg-num)
+
+; An if statement to determine if a number is positive
+; If statement takes 3 arguments
+; 1: Is the argument to test against
+; 2: Is the argument that returns true
+; 3: Is the argument that returns false
+(if (positive? neg-num) (error "Number is not positive") success)
