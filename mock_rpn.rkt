ba425bc7aa466cdb588caaf0c73e5863f0aae648
Christy <christymadden80@gmail.com>
2017-04-22 13:32:54 +0100

    Add comments to explain how function should work in final version

--- mock_rpn.rkt
+++ mock_rpn.rkt
@@ -17,7 +17,7 @@
 (define start-perm (list -1 -1 -1 -1 1 1 1 1))
 
 ; 40320 permutations with the 8 element list
-(length (permutations start-perm))
+;(length (permutations start-perm))
 
 ; 70 permutations When duplicates are removed 
 ;(length (remove-duplicates (permutations start-perm)))
@@ -35,7 +35,7 @@
 ;(length (map make-rpn dup-free))
 
 ; Show all the permutations which start and end correctly
-(map make-rpn dup-free)
+;(map make-rpn dup-free)
 
 ;(null? dup-free)
 
@@ -44,12 +44,22 @@
 ; valid-rpn takes 2 arguments
 ; second argument is stack or set to 0 as default value
 ; optional arguement to the func with default value of 0
-#|(define (valid-rpn? e [s 0])) ; e is duplicate free list 
+#|(define (valid-rpn? e [s 0])) ; e is duplicate free list, s is the stack 
    (if(null? e)    ; check if list is null
-     (if (= s 1) #t #f) ; 
+
+     ; This won't work in final version
+     ; It's only true is values are the exact same
+     ; Here it's mock testing if both elements are numbers
+     ; In final version the test is to check if bot elements are numbers E.G 12 and 34
+     ; The other scenario is that one or both elements are operators
+     (if (= s 1) #t #f) ; What element of s is being compared??????
+
+      ;if the first thing on e is 1
      (if(= (car e) 1) ; if top element of dup free list is equal to 1 (A Number)
-         ;if the first thing on e is 1
-         (valid-rpn? (cdr e)(+ 1 s))
+
+        
+         (valid-rpn? (cdr e)(+ 1 s)) ; cdr extracts second element in list
+
          ;fill this part in
          )))
 |#
@@ -68,6 +78,17 @@
 ; If dup-free list 
 ;(valid-rpn? dup-free)
 
+; Define two numbers and test whether they are the same type
+(define num1 10)
+(define num2 20)
+(define number-string "number")
+
+; Define an operator to test against a number
+(define op1 '+)
+
+; If statement to test how the equals evalutes numbers
+; Result: = operator tests if number are the exact same value
+(if (= num1 num2) #t #f)
 
 
 
