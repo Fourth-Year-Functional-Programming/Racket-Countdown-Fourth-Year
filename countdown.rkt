a370d3a30e6b82d2c12b9932b385b3b882a317bd
Christy <christymadden80@gmail.com>
2017-04-22 19:56:37 +0100

    Add next steps to take

--- countdown.rkt
+++ countdown.rkt
@@ -21,10 +21,26 @@
 
 (length dup-free)
 
+; Could make this function recursive and run it with the different values
+; E.G (list 1 2) l (list '-)
+;     (list 1 2) l (list '/)
+;     (list 2 1) l (list '-)
+;     (list 2 1) l (list '/)
+;
+; Then remove duplicates (Will be slow program though)
+
 ; Need to perform this function many times
 ; As list also starts and ends with the same elements
 (define (make-rpn l)
   (append (list 1 2) l (list '+)))
 
 ; Mix lists together
+; 40320 permutations
 (map make-rpn dup-free)
+
+; **********  NEXT TO DO  **********
+
+; Need to define stack here
+
+; Need to make function to check and evalute the permutations
+; and see which ones are valid
